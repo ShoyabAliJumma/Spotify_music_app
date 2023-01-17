3bd1617c2922ecd6d879c3336b18df44c1f47db5
ShoyabAliJumma <shoyabalipathan@gmail.com>
2023-01-18 02:06:19 +0530

    Spotify_music_app

---
+++ src/Song.java
@@ -0,0 +1,34 @@
+public class Song {
+
+    String  title;
+    Double duration;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Double duration) {
+        this.duration = duration;
+    }
+
+    public Song(String title, Double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+
+
+}
